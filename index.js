d1296613d9e3eed47346b9402a4e2815d3992be8
Pyry Pajunpää <pymapa@utu.fi>
2017-02-21 20:09:59 +0200

    index.html added, socket working

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ var io = require('socket.io')(server);
 var logger = require('morgan');
 var path = require('path');
 
-
-
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 
 //Serve static files from public-folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use(express.static('views'));
 
+io.sockets.on('connection', function(socket) {
+    console.log('socket connected!!!');
 
+    socket.on('disconnect', function() {
+        console.log("disconnected");
+    })
+})
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+})
 
 server.listen(app.get('port'), function() {
     console.log("Server running on port " + app.get('port'));
