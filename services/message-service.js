43abf38568b61338774f2613aa504963a8d15106
Pyry Pajunpää <pymapa@utu.fi>
2017-02-28 18:16:17 +0200

    s

--- services/message-service.js
+++ services/message-service.js
@@ -1,35 +1,35 @@
 var Message = require('./../models/message-model');
 
-module.exports = function () {
+module.exports = {
 
-    getMessages = function (callback) {
+    getMessages: function (callback) {
         Message.find({})
             .then(function (data) {
                 callback(false, data);
-            }, function(error) {
+            }, function (error) {
                 callback(true, error)
             })
-    }
+    },
 
     // Save message. callback false if success, true if error
     // Return message _id
-    newMessage = function (data, callback) {
-        if(!data.lecture) data.lecture = "";
+    newMessage: function (data, callback) {
+        if (!data.lecture) data.lecture = "";
         var message = new Message({
             message: data.message,
             course: data.course,
             lecture: data.lecture,
             user: data.user,
             category: data.category
-    });
+        });
         message.save()
-        .then(function(data) {
-            console.log("message saved");
-            console.log(data._id);
-            callback(false, message);
-        }, function(error) {
-            callback(true, error);
-        })
+            .then(function (data) {
+                console.log("message saved");
+                console.log(data._id);
+                callback(false, message);
+            }, function (error) {
+                callback(true, error);
+            })
     }
 
 }
